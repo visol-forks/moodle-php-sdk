06b7f8aa8cd993ff82c2645aba1719b4326242f1
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2019-05-15 16:34:16 +0000

    [WIP][TASK] Use Guzzle

--- src/Rest/RestApiCall.php
+++ src/Rest/RestApiCall.php
@@ -2,6 +2,8 @@
 
 use MoodleSDK\Api\ApiCall;
 use MoodleSDK\Log\ConsoleLog;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 
 class RestApiCall implements ApiCall {
 
@@ -13,37 +15,61 @@ class RestApiCall implements ApiCall {
     private $url;
 
     public function __construct($url, $method, array $payload) {
-        if (!function_exists('curl_init')) {
-            throw new Exception('cURL module must be enabled');
-        }
-
         $this->url = $url;
         $this->method = $method;
         $this->payload = $payload;
     }
 
     public function execute() {
-        $curl = curl_init();
-        $payloadQueryString = http_build_query($this->payload);
+        $client = new Client([
+            'base_uri' => $this->url,
+        ]);
 
-        if ($this->getDebug()) {
-            ConsoleLog::i()
-                ->section('cURL request: POST '.$this->url)
-                ->info($this->url.'&wsfunction='.$this->method.'&'.$payloadQueryString);
-        }
+        var_dump($this->payload);
+        var_dump($this->method);
+
+        $requestUrl = $this->url . '&wsfunction=' . $this->method; // . '&' . http_build_query($this->payload);
+
+        /** @var Response $response */
+        $response = $client->post($requestUrl, [
+            'headers' => [
+                'Content-Type' => 'application/json'
+            ],
+            'curl' => [
+                CURLOPT_USERAGENT => 'agurz/moodle-php-sdk',
+                CURLOPT_CONNECTTIMEOUT => 30,
+                CURLOPT_SSL_VERIFYHOST => 2,
+                CURLOPT_SSL_VERIFYPEER => false, // see http://unitstep.net/blog/2009/05/05/using-curl-in-php-to-access-https-ssltls-protected-sites/
+            ],
+            'json' => $this->payload
+        ]);
+
+
+        var_dump((string)$response->getBody());
+        die();
+
+
+        //$payloadQueryString = http_build_query($this->payload);
+
+        \Neos\Flow\var_dump($this->url);
+        \Neos\Flow\var_dump($payloadQueryString);
+
+//        if ($this->getDebug()) {
+//            ConsoleLog::i()
+//                ->section('cURL request: POST '.$this->url)
+//                ->info($this->url.'&wsfunction='.$this->method.'&'.$payloadQueryString);
+//        }
         
         curl_setopt_array($curl, [
             CURLOPT_URL => $this->url.'&wsfunction='.$this->method.'&'.$payloadQueryString,
-            CURLOPT_USERAGENT => 'agurz/moodle-php-sdk',
             CURLOPT_HTTPHEADER => [
                 'Content-Type: text/plain',
             ],
-            CURLOPT_POST => 1,
-            CURLOPT_POSTFIELDS => $payloadQueryString,
-            CURLOPT_RETURNTRANSFER => 1,
+//            CURLOPT_POST => 1,
+//            CURLOPT_POSTFIELDS => $payloadQueryString,
+//            CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_SSL_VERIFYHOST => 2,
             CURLOPT_SSL_VERIFYPEER => false, // see http://unitstep.net/blog/2009/05/05/using-curl-in-php-to-access-https-ssltls-protected-sites/
-            CURLOPT_CONNECTTIMEOUT => 30
         ]);
 
         $response = curl_exec($curl);
@@ -51,13 +77,13 @@ class RestApiCall implements ApiCall {
         $info = curl_getinfo($curl);
         $err = curl_error($curl);
 
-        if ($this->getDebug()) {
-            ConsoleLog::i()
-                ->section('cURL response')
-                ->info($response)
-                ->infoAnx('INFO: '.json_encode($info))
-                ->infoAnx('ERR: '.$err);
-        }
+//        if ($this->getDebug()) {
+//            ConsoleLog::i()
+//                ->section('cURL response')
+//                ->info($response)
+//                ->infoAnx('INFO: '.json_encode($info))
+//                ->infoAnx('ERR: '.$err);
+//        }
 
         curl_close($curl);
 
